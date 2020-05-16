74eb8709a7db372d96de1485bbf6334d23e775d2
Stephen Harris <contact@stephenharris.info>
2020-05-16 14:26:26 +0100

    add async

--- src/message-gateway.ts
+++ src/message-gateway.ts
@@ -10,7 +10,7 @@ export class MessageGateway {
     private emitState(client, game: Uno) {
      this.gameService.getPlayers(game.id())
       .then(async (players) => {
-        players.map((player) => {
+        players.map(async (player) => {
           console.log(`send state to ${player.playerId} / ${player.connectionId}`);
         
           let payload = game.getPlayerState(player.playerId);
